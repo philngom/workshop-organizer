34313d6afe699926d35a5c6088ce2a32e9650c8e
Philippe Ngom <philngom@gmail.com>
2022-01-11 16:54:07 -0800

    added displayWorkshops function. able to get workshops from db and render on browser

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -4,9 +4,10 @@ import {
     getWorkshops
 } from '../fetch-utils.js';
 
-checkAuth();
-
 const logoutButton = document.getElementById('logout');
+const workshopsContainerEl = document.querySelector('.workshops-container');
+
+checkAuth();
 
 logoutButton.addEventListener('click', () => {
     logout();
@@ -15,4 +16,22 @@ logoutButton.addEventListener('click', () => {
 window.addEventListener('load', async() => {
     const workshops = await getWorkshops();
 
+    displayWorkshops(workshops);
 });
+
+function displayWorkshops(workshops) {
+
+    workshopsContainerEl.textContent = '';
+
+    for (let workshop of workshops) {
+        console.log(workshop);
+        const workshopContainerEl = document.createElement('div');
+        const workshopNameEl = document.createElement('h5');
+
+        workshopNameEl.classList.add('workshop');
+        workshopNameEl.textContent = workshop.name;
+
+        workshopContainerEl.append(workshopNameEl);
+        workshopsContainerEl.append(workshopContainerEl);
+    }
+}
